5e1ada723eaedb74dc1c0525168d03edaaad1665
donald <dfosterstewart@gmail.com>
2022-02-27 12:19:23 -0800

    EARWebsite first commit

---
+++ scripts/script.js
@@ -0,0 +1,59 @@
+//--------- MOBILE NAVBAR ----------
+let navList = document.getElementById("navlist");
+let toggleButton = document.getElementById("navHamburger");
+toggleButton.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
+// Switch Covers
+function switchCoverITSOTF() {
+  var image = document.getElementById("frontImage");
+  if (image.src.match("assets/images/coverFront-itsotf.jpg")) {
+    image.src = "assets/images/cover-rwlyf.jpg";
+  } else {
+    image.src = "assets/images/coverFront-itsotf.jpg";
+  }
+}
+
+function switchCoverSERENDIPITY() {
+  var image = document.getElementById("frontImage");
+  if (image.src.match("assets/images/coverFront-itsotf.jpg")) {
+    image.src = "assets/images/cover-rwlyf.jpg";
+  } else {
+    image.src = "assets/images/coverFront-itsotf.jpg";
+  }
+}
+
+function switchCoverTFYTF() {
+  var image = document.getElementById("frontImage");
+  if (image.src.match("assets/images/coverFront-itsotf.jpg")) {
+    image.src = "assets/images/cover-rwlyf.jpg";
+  } else {
+    image.src = "assets/images/coverFront-itsotf.jpg";
+  }
+}
+
+function switchCoverRWLYF() {
+  var image = document.getElementById("frontImage");
+  if (image.src.match("assets/images/coverFront-itsotf.jpg")) {
+    image.src = "assets/images/cover-rwlyf.jpg";
+  } else {
+    image.src = "assets/images/coverFront-itsotf.jpg";
+  }
+}
+
+function switchCoverACD() {
+  var image = document.getElementById("frontImage");
+  if (image.src.match("assets/images/coverFront-itsotf.jpg")) {
+    image.src = "assets/images/cover-rwlyf.jpg";
+  } else {
+    image.src = "assets/images/coverFront-itsotf.jpg";
+  }
+}
+function switchCoverTFYTFold() {
+  var image = document.getElementById("frontImage");
+  if (image.src.match("assets/images/coverFront-itsotf.jpg")) {
+    image.src = "assets/images/cover-rwlyf.jpg";
+  } else {
+    image.src = "assets/images/coverFront-itsotf.jpg";
+  }
+}
