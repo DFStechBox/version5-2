7d550b71460a4d77900596535887f350d5f6d78a
quantum49 <quantum49@users.noreply.github.com>
2022-03-03 09:56:25 -0800

    update cover switch images

--- scripts/script.js
+++ scripts/script.js
@@ -6,54 +6,54 @@ toggleButton.addEventListener("click", () => {
 });
 // Switch Covers
 function switchCoverITSOTF() {
-  var image = document.getElementById("frontImage");
+  var image = document.getElementById("itsotf-book");
   if (image.src.match("assets/images/coverFront-itsotf.jpg")) {
-    image.src = "assets/images/cover-rwlyf.jpg";
+    image.src = "assets/images/itsotf-backCover.jpg";
   } else {
     image.src = "assets/images/coverFront-itsotf.jpg";
   }
 }
 
 function switchCoverSERENDIPITY() {
-  var image = document.getElementById("frontImage");
-  if (image.src.match("assets/images/coverFront-itsotf.jpg")) {
-    image.src = "assets/images/cover-rwlyf.jpg";
+  var image = document.getElementById("serendipity-book");
+  if (image.src.match("assets/images/coverFront-serendipity.jpg")) {
+    image.src = "assets/images/coverBack-serendipity-82p.jpg";
   } else {
-    image.src = "assets/images/coverFront-itsotf.jpg";
+    image.src = "assets/images/coverFront-serendipity.jpg";
   }
 }
 
 function switchCoverTFYTF() {
-  var image = document.getElementById("frontImage");
-  if (image.src.match("assets/images/coverFront-itsotf.jpg")) {
-    image.src = "assets/images/cover-rwlyf.jpg";
+  var image = document.getElementById("tfytf-book-new");
+  if (image.src.match("assets/images/coverFront-tfytf-new.jpg")) {
+    image.src = "assets/images/tfytfBack-new.jpg";
   } else {
-    image.src = "assets/images/coverFront-itsotf.jpg";
+    image.src = "assets/images/coverFront-tfytf-new.jpg";
   }
 }
 
 function switchCoverRWLYF() {
-  var image = document.getElementById("frontImage");
-  if (image.src.match("assets/images/coverFront-itsotf.jpg")) {
-    image.src = "assets/images/cover-rwlyf.jpg";
+  var image = document.getElementById("rwlyf-book");
+  if (image.src.match("assets/images/coverFront-rwlyf.jpg")) {
+    image.src = "assets/images/coverBack-rwlyf.jpg";
   } else {
-    image.src = "assets/images/coverFront-itsotf.jpg";
+    image.src = "assets/images/coverFront-rwlyf.jpg";
   }
 }
 
 function switchCoverACD() {
-  var image = document.getElementById("frontImage");
-  if (image.src.match("assets/images/coverFront-itsotf.jpg")) {
-    image.src = "assets/images/cover-rwlyf.jpg";
+  var image = document.getElementById("acd-book");
+  if (image.src.match("assets/images/coverFront-acd.jpg")) {
+    image.src = "assets/images/coverBack-acd.jpg";
   } else {
-    image.src = "assets/images/coverFront-itsotf.jpg";
+    image.src = "assets/images/coverFront-acd.jpg";
   }
 }
 function switchCoverTFYTFold() {
-  var image = document.getElementById("frontImage");
-  if (image.src.match("assets/images/coverFront-itsotf.jpg")) {
-    image.src = "assets/images/cover-rwlyf.jpg";
+  var image = document.getElementById("tfytf-book-old");
+  if (image.src.match("assets/images/coverFront-tfytf-old.jpg")) {
+    image.src = "assets/images/coverBack-tfytf-old.jpg";
   } else {
-    image.src = "assets/images/coverFront-itsotf.jpg";
+    image.src = "assets/images/coverFront-tfytf-old.jpg";
   }
 }
